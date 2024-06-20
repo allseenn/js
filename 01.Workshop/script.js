f6158af91da364970e63f6aa2b4380e9dc358c8b
allseenn <allseen@live.ru>
2024-06-20 21:49:36 +0000

    01.Workspace

---
+++ 01.Workshop/script.js
@@ -0,0 +1,40 @@
+// const num = 123;
+// console.log(num);
+
+// let a = 5;
+// a = 7;
+// console.log(a);
+
+// let b = 1+2+3;
+// console.log(b);
+
+/*hello*/
+//world
+
+// const c = 10;
+// const d = 2;
+// let sumCD = c + d;
+// let differenceCD = c - d;
+// let multiplyCD = c * d;
+// let divisionCD = c / d;
+// console.log(`sum of ${c} and ${d} = ${sumCD}
+// difference of ${c} and ${d} = ${differenceCD}
+// multiply of ${c} and ${d} = ${multiplyCD}
+// division of ${c} and ${d} = ${divisionCD}`); 
+
+// const a = 1.5;
+// const b = 0.75;
+// let sumAB = a + b;
+// console.log(`sum of ${a} and ${b} = ${sumAB}`);
+// const c = 5;
+// console.log(`c = ${-c}`);
+
+const firstName = "Rostislav";
+const lastName = "Romashin";
+console.log(`My name is ${firstName} ${lastName}`);
+const java = "java";
+const script = "script";
+console.log(java + script);
+const hello = "hello";
+const world = "world";
+console.log(hello + " " + world);
