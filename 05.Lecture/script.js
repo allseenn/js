cdbf3f8af7482e891f465709c1b33f0f8be47179
allseenn <allseen@live.ru>
2024-06-27 14:09:37 +0300

    05.Lecture

---
+++ 05.Lecture/script.js
@@ -0,0 +1,70 @@
+// function helloName(name) {
+//     console.log(`Hello, ${name}`);
+// }
+
+// helloName('John');
+
+// let count = 5;
+
+// function counter() {
+//     return count++;
+// }
+
+// counter();
+// console.log(count);
+// let age = +prompt("Ваш возраст: ", 30);
+// function upAge() {
+//     return age+5;
+// }
+// console.log(`After 5 years you will be ${upAge()}`);
+
+// let age = +prompt("Ваш возраст: ", 30);
+// const lvUpAge = () => age + 5;
+// console.log(`After 5 years you will be ${lvUpAge()}`);
+
+// function hello() {
+//     console.log("Hello");
+// }
+
+// hello();
+
+// const sum = (param1, param2) => {
+//     return param1 + param2;
+// }
+
+// console.log(sum(2,5));
+
+// const salary = (money) => (money *= 0.87)*0.75;
+
+// let userMoney = +prompt("Ваша зарплата: ", 100);
+
+// let monthMoney = salary(userMoney);
+
+// console.log(`Ваша остаток: ${monthMoney}`);
+
+// console.log(`На еду можно тратить ${monthMoney *0.3}`);
+
+// const hello2 = () => {
+//     alert("Hello2");
+// }
+// hello2();
+
+// function sayHello() {
+//     alert("Hello");
+//     alert("You pressed button");
+// }
+
+function askQuestion(answer ,question) {
+    let userAnswer = prompt(question);
+    if (userAnswer.toLowerCase() === answer) 
+        alert('Правильно!');  
+    else 
+        alert('Неправильно!');
+}
+
+
+function puzzles() {
+    askQuestion('лук', 'Сидит дед, во 100 шуб одет, кто его раздевает, тот слезы приливает!');
+    askQuestion('елка', 'Зимой и летом одним цветом!');
+    
+}
