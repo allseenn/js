12609821f9c9f1be772abfc12e9849d693207aea
allseenn <allseen@live.ru>
2024-07-01 22:26:48 +0000

    08

---
+++ 08.Seminar/script.js
@@ -0,0 +1,124 @@
+// if((0.8 - 0.1) !== 0.7){
+//     console.log(true)
+// }
+// const arr1 = [1, 2, 3]
+// console.log(arr1.join('\n'))
+
+// const arr2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// console.log(arr2.length)
+
+// const arr3 = ['a', 'b', 'c']
+// arr3[0] = 1
+// arr3[1] = -2
+// arr3[2] = -3
+// console.log(arr3)
+
+// function random(amount){
+//     const arr = []
+//     for(let i = 0; i < amount; i++){
+//         arr.push(Math.round(Math.random()*10))
+//     }
+//     return arr
+// }
+
+// arr4 = random(9)
+// console.log(arr4)
+// console.log(arr4.length)
+
+// const arr5 = ['a', 'b', 'c', 'd']
+// for (let i = 0; i < arr5.length; i++) {
+//     arr5[i] = i;
+// }
+// console.log(arr5)
+
+// const arr1 = [1, 2, 3]
+// arr1[0]++
+// arr1[1]++
+// arr1[2]++
+// console.log(arr1)
+
+// const arr = []
+// arr[3] = 'a'
+// arr[8] = 'b'
+// console.log(arr.length)
+
+// const arr3 = [1, 2, 3]
+// arr3.push(4, 5)
+// console.log(arr3)
+
+// const arr4 = [1, 2, 3, 4, 5]
+// const arr5 = arr4.slice(0, 3)
+// console.log(arr5)
+// console.log(arr5.length)
+
+// for (let i = 11; i < 34; i++) {
+//     console.log(i)
+// }
+
+// for (let i = 1; i < 100; i++) {
+//     if (i % 2 !== 0)
+//         console.log(i)
+// }
+
+// for (let i = 100; i >= 0; i--) {
+//     console.log(i)
+// }
+
+// num = +prompt("Enter number: ")
+// count = 0;
+// while (true) {
+//     if (num > 1000)
+//         break
+//     num*=3
+//     count++
+// }
+// console.log(count)
+
+// for (let i = 1; i < 4; i++) {
+//     const arra = []
+//     for (let j = 1; j < 4; j++) {
+//         arra.push(i*10+j)
+//     }
+//     console.log(arra.join(' '))
+// }
+
+// const arr = [2, 5 , 9, 15, 1, 4]
+// for (let i = 0; i < arr.length; i++) {
+//     if(arr[i] > 3 && arr[i] < 10)
+//     console.log(arr[i]);
+// }
+
+// console.log(arr.filter(item => item > 3 && item < 10));
+
+// let summa = 0
+// for (let i = 2; i <= 100; i++) {
+//     if(i % 2 === 0)
+//         summa += i
+// }
+// console.log(summa)
+
+
+// summa = 0
+// const arr2 = [2, 5, 9, 3, 1, 4]
+// for (let i = 0; i < arr2.length; i++) {
+//     summa += arr2[i]
+// }
+// console.log(summa)
+
+// arr2.reduce((a, b) => a + b)
+// console.log(arr2)
+
+// let row = ''
+// for (let i = 0; i < 10; i++) {
+//     row += `-${i}`
+// }
+// console.log(`${row}-`)
+
+// const arr3 = [2, 5, 9, 0, 3, 1, 4]
+// for (let i = 0; i < arr3.length; i++) {
+//     console.log(arr3[i])
+//     if(arr3[i] === 0)
+//         break
+    
+// }
+
