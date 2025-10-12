715cf27ea1e5ee2ec25dd8a010af7c8f5f6978a6
allseenn <allseen@live.ru>
2025-10-12 23:59:08 +0300

    ~README.md -test.js webserial 17

--- test.js
+++
@@ -1,4 +0,0 @@
-new Promise(resolve => setTimeout(() => resolve(1), 1000))
-    .then(result => new Promise(resolve => setTimeout(() => resolve(result + 1), 1000)))
-    .then(result => new Promise(resolve => setTimeout(() => resolve(result + 1), 1000)))
-    .then(console.log);
