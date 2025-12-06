a45e4c87f1ef259b3ba37f10e621e9dfc0ae8a22
allseenn <allseen@live.ru>
2025-12-06 21:11:20 +0000

    promises

--- index.mjs
+++ index.mjs
@@ -1,14 +1,3 @@
-class Thenable {
-  constructor(num) {
-    this.num = num;
-  }
-  then(resolve) {
-    setTimeout(() => resolve(this.num + 1), 1000);
-  }
-}
-
-new Promise(resolve => resolve(1))
-  .then(result => new Thenable(result))      // (1)
-  .then(result => new Thenable(result))      // (2)
-  .then(console.log);                       // (3)
-
+console.log("hello")
+setTimeout(() => console.log("Tick"), 500);
+console.log("bye")
